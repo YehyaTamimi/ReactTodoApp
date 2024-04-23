3c6a5994b13dc731099a78d859b58ea070c1c280
YehyaTamimi <tamimiyehya@gmail.com>
2024-04-23 11:37:19 +0300

    remove comments

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -1,27 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./Card.css"
 
-// export default function TodoCard ({ text }) {
-//     const [checked, SetChecked] = useState(false);
-
-//     const changeCardState = () => {
-//         SetChecked(!checked);
-//     }
-
-//     return (
-//         <div className='card'>
-//             <label className="container">
-//                 <span className={checked ? "crossed" : ""}>{text}</span>
-//                 <input type="checkbox" checked={checked} onChange={changeCardState} />
-//                 <span className="checkmark"></span>
-//             </label>
-//             <button className='edit'><i className="fa-solid fa-pen-to-square"></i></button>
-//             <button className='delete'><i className="fa-solid fa-trash"></i></button>
-//         </div>
-//     )
-// }
-
-
 export default function TodoCard ({text , id, deleteCard}) {
     const [checked, SetChecked] = useState(false);
     const [cardText, setCardText] = useState(text);
